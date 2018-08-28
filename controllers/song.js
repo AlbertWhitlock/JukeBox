ab008f3179809b5cca5f932add2caa36e2423fed
Albert <albertwhitlock4@gmail.com>
2018-08-28 17:57:20 -0400

    Progress with comments

--- controllers/song.js
+++ controllers/song.js
@@ -2,54 +2,67 @@ const { Song, Comment } = require("../models/Song");
 const User = require("../models/User");
 
 module.exports = {
-    index: (req, res) => {
-        console.log("Inside index. Outside the then");
-        Song.find({})
-        .then( songs => {
-            console.log("Inside index. Inside the then");
-            res.render("index", { songs })
-        })
-    },
-    show: (req, res) => {
-        Song.findOne({ _id: req.params.id })
-            .populate("author")
-            .exec(function (err, song) {
-                Comment.populate(song.comments, { path: "author" }, function (err, comments) {
-                    song.comments = comments;
-                    res.render("show", song);
-                });
-            });
-    },
-    new: (req, res) => {
-     //   Song.find({}).then(users => {
-            res.render("song/new");
-      //  });
-    },
-    create: (req, res) => {
-        Song.create({
-            name: req.body.song.name,
-            artist: req.body.song.artist,
-            year: req.body.song.year,
-            genre: req.body.song.genre
-        }).then(song => {
-                //req.user.songs.push(song);
-                //req.user.save(err => {
-                    res.render("song/show", { song })
-                //});
+  index: (req, res) => {
+    console.log("Inside index. Outside the then");
+    Song.find({}).then(songs => {
+      console.log("Inside index. Inside the then");
+      res.render("index", { songs });
+    });
+  },
+
+  //   show: (req, res) => {
+  //     console.log('get gif: ', req.params.id)
+  //     Gif.findOne({ _id: req.params.id })
+  //       .populate("author")
+  //       .populate("comments.author")
+  //       .then(gif => {
+  //         res.render('gif/show', gif)
+  //       })
+  //   },
+
+  show: (req, res) => {
+    Song.findOne({ _id: req.params.id })
+      //.populate("author")
+      .exec(function(err, song) {
+        Comment.populate(song.comments, { path: "author" }, function(
+          err,
+          comments
+        ) {
+          song.comments = comments;
+          res.render("song/show", song);
         });
-    },
-    update: (req, res) => {
-        let { name, artist, year, genre } = req.body;
-        Song.findOne({ _id: req.params.id }).then(song => {
-            song.comments.push({ name, artist, year, genre });
-            song.save(err => {
-                res.redirect(`/song/${song._id}`);
-            });
-        });
-    },
-    delete: (req, res) => {
-        Song.findOneAndRemove({ _id: req.params.id }).then(song => {
-            res.redirect('/');
-        });
-    }
+      });
+  },
+  new: (req, res) => {
+    //   Song.find({}).then(users => {
+    res.render("song/new");
+    //  });
+  },
+  create: (req, res) => {
+    Song.create({
+      name: req.body.song.name,
+      artist: req.body.song.artist,
+      year: req.body.song.year,
+      genre: req.body.song.genre
+    }).then(song => {
+      //req.user.songs.push(song);
+      //req.user.save(err => {
+      res.render("song/show", { song });
+      //});
+    });
+  },
+  update: (req, res) => {
+    let { name, artist, year, genre } = req.body;
+    Song.findOne({ _id: req.params.id }).then(song => {
+      song.comments.push({ name, artist, year, genre });
+      song.save(err => {
+        res.redirect(`/song/${song._id}`);
+      });
+    });
+  },
+  delete: (req, res) => {
+    Song.findOneAndRemove({ _id: req.params.id }).then(song => {
+      res.redirect("/");
+    });
+  }
 };
--- models/Song.js
+++ models/Song.js
@@ -2,26 +2,24 @@ const mongoose = require("../db/connection");
 const Schema = mongoose.Schema;
 
 const Comment = new Schema({
-    content: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-})
-
+  content: String
+  //author: {
+  //    type: Schema.Types.ObjectId,
+  //    ref: "User"
+});
 
 const Song = new Schema({
-    name: String,
-    artist: String,
-    year: Number,
-    genre: String,
-    author: {
-        type: Schema.Types.ObjectId,
-        ref: "User"
-    }
-})
+  name: String,
+  artist: String,
+  year: Number,
+  genre: String,
+  //author: {
+  //    type: Schema.Types.ObjectId,
+  //    ref: "User"
+  comments: [Comment]
+});
 
 module.exports = {
-    Song: mongoose.model("Song", Song),
-    Comment: mongoose.model("Comment", Comment)
+  Song: mongoose.model("Song", Song),
+  Comment: mongoose.model("Comment", Comment)
 };
