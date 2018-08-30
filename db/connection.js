997424149be27b04a78eb0d947d492f00e2e09a7
Albert <albertwhitlock4@gmail.com>
2018-08-30 14:40:09 -0400

    Deployment steps

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/songs");
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MLAB_URL);
+} else {
+  mongoose.connect("mongodb://localhost/songs");
+}
 mongoose.Promise = Promise;
 module.exports = mongoose;
